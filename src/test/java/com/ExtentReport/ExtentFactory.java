603572847c2a64b07a1e47e59d1fc0112c556f70
mceciabate <m.cecilia.abate@gmail.com>
2023-03-11 16:44:57 -0300

    reportes

---
+++ src/test/java/com/ExtentReport/ExtentFactory.java
@@ -0,0 +1,13 @@
+package com.ExtentReport;
+
+import com.aventstack.extentreports.ExtentReports;
+
+public class ExtentFactory {
+
+    public static ExtentReports getInstance() {
+        ExtentReports extentReports = new ExtentReports();
+        extentReports.setSystemInfo("Selenium version", "4.0");
+        extentReports.setSystemInfo("SO", "Windows 10");
+        return extentReports;
+    }
+}
--- src/test/java/com/Tests/DigitalBookingCrearCuentaRefactor.java
+++ src/test/java/com/Tests/DigitalBookingCrearCuentaRefactor.java
@@ -1,6 +1,12 @@
 package com.Tests;
 
+import com.ExtentReport.ExtentFactory;
 import com.Pages.RegistrationPage;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -20,6 +26,12 @@ public class DigitalBookingCrearCuentaRefactor {
 
     static WebDriver driver;
     static WebDriverWait wait;
+    //REPORTE
+    static ExtentSparkReporter spark = new ExtentSparkReporter("target/Spark.html");
+    static ExtentReports report;
+
+    ExtentTest test;
+
 
 
 
@@ -28,13 +40,17 @@ public class DigitalBookingCrearCuentaRefactor {
         driver = RegistrationPage.setup();
         RegistrationPage.url("http://testing.ctd.academy/registro");
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-
+        report = ExtentFactory.getInstance();
+        report.attachReporter(spark);
 
     }
 
     @Test
 
     public void crearCuenta(){
+
+        test = report.createTest("Create account");
+        test.log(Status.INFO, "INICIA EL TEST");
         //capturo los elementos en una lista y espero que sean visibles
         ArrayList<WebElement> elementosPresentes = new ArrayList<>();
         elementosPresentes = (ArrayList<WebElement>) RegistrationPage.obtenerElementosFormulario();
@@ -43,14 +59,20 @@ public class DigitalBookingCrearCuentaRefactor {
         //limpio los campos de los elementos de la lista
         RegistrationPage.limpiarCampos(elementosPresentes);
         //completo los campos
-        RegistrationPage.completarCampos("Juan", "Perez", "mariaceciliaabate@mail.com", "123456", "123456");
+        test.log(Status.PASS, "Se capturan y visualizan los elementos");
+        RegistrationPage.completarCampos("Juan", "Perez", "mariaceciliaabate2@mail.com", "123456", "123456");
+        test.log(Status.PASS, "Se completaron los campos");
         //submit
         RegistrationPage.enviarForm();
         //espero a que cambie la url y la capturo
         /*PAUTA IMPLICITA*/
+
+        test.addScreenCaptureFromPath("../resources/capturas", "creacion_cuenta.jpg" );
         boolean envioExitoso = wait.until(ExpectedConditions.urlToBe("http://testing.ctd.academy/registro-exitoso"));
 
         assertTrue(envioExitoso);
+        test.log(Status.INFO, "se terminó el test");
+
 
 
     }
@@ -65,7 +87,7 @@ public class DigitalBookingCrearCuentaRefactor {
         //limpio los campos de los elementos de la lista
         RegistrationPage.limpiarCampos(elementosPresentes);
         //completo los campos
-        RegistrationPage.completarCampos("Juan", "Perez", "cecilia0@mail.com", "123456", "123456");
+        RegistrationPage.completarCampos("Juan", "Perez", "mariaceciliaabate1@mail.com", "123456", "123456");
         //submit
         RegistrationPage.enviarForm();
 
@@ -83,6 +105,7 @@ public class DigitalBookingCrearCuentaRefactor {
 
     @AfterAll
     public void salir(){
+        report.flush();
         RegistrationPage.salir();
     }
 
